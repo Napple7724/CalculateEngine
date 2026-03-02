958ab8d271200a32d42ae89fad135d7d52589ea1
Napple7724 <napple7724@nineapple.xyz>
2026-03-02 14:16:43 +0900

    약수 및 배수 구하기 기능

--- calculate_engine.py
+++ calculate_engine.py
@@ -7,22 +7,65 @@
 ## Thank you!
 
 
-def allcalc(str_sign, list_int_num, bool_show_err_message) :
-    int_answer = list_int_num[0]
-    if str_sign == "+" :
-        int_answer = sum(list_int_num)
-    elif str_sign in ["-", "*", "/", "^"] :
-        for sys_int_r1 in list_int_num[1::] :
-            if str_sign == "-" :
-                int_answer -= sys_int_r1
-            elif str_sign == "*" :
-                int_answer *= sys_int_r1
-            elif str_sign == "/" :
-                int_answer /= sys_int_r1
+def allcalc(sign, numbers, show_err_msg = True) :
+    if type(numbers) != list :
+        if show_err_msg :
+            print("Calculate Engine ERROR! : 인수 \"numbers\"(은)는 list 형태로 입력되어야 합니다.")
+        return None
+    if type(show_err_msg) != bool :
+        if show_err_msg :
+            print("Calculate Engine ERROR! : 인수 \"show_err_msg\"(은)는 bool 형태로 입력되어야 합니다.")
+        return None
+    int_answer = numbers[0]
+    if sign == "+" :
+        int_answer = sum(numbers)
+    elif sign in ["-", "*", "/", "^"] :
+        for r1 in numbers[1::] :
+            if sign == "-" :
+                int_answer -= r1
+            elif sign == "*" :
+                int_answer *= r1
+            elif sign == "/" :
+                int_answer /= r1
             else :
-                int_answer **= sys_int_r1
+                int_answer **= r1
     else :
-        if bool_show_err_message == True :
-            print("Calculate Engine ERROR! : The sign you entered couldn't be found.\n계산 엔진 오류! : 입력하신 부호를 찾을 수 없습니다.")
+        if show_err_msg :
+            print(f"Calculate Engine ERROR! : 인수 \"sign\"(은)는 다음 중 하나여야 합니다 : {['+', '-', '*', '/', '^']}")
+
         return None
     return int_answer
+
+def yagseu(number, show_err_msg=True) :
+    if type(number) != int :
+        if show_err_msg :
+            print("Calculate Engine ERROR! : 인수 \"number\"(은)는 int 형태로 입력되어야 합니다.")
+        return None
+    if type(show_err_msg) != bool :
+        if show_err_msg :
+            print("Calculate Engine ERROR! : 인수 \"show_err_msg\"(은)는 bool 형태로 입력되어야 합니다.")
+        return None
+    list_answer = []
+    for r1 in range(1, number // 2 + 1) :
+        if number % r1 == 0 :
+            list_answer.append(r1)
+    list_answer.append(number)
+    return list_answer\
+
+def baeseu(number, limit, show_err_msg=True) :
+    if type(number) != int :
+        if show_err_msg :
+            print("Calculate Engine ERROR! : 인수 \"number\"(은)는 int 형태로 입력되어야 합니다.")
+        return None
+    if type(limit) != int :
+        if show_err_msg :
+            print("Calculate Engine ERROR! : 인수 \"limit\"(은)는 int 형태로 입력되어야 합니다.")
+        return None
+    if type(show_err_msg) != bool :
+        if show_err_msg :
+            print("Calculate Engine ERROR! : 인수 \"show_err_msg\"(은)는 bool 형태로 입력되어야 합니다.")
+        return None
+    list_answer = []
+    for r1 in range(1, limit + 1) :
+        list_answer.append(number * r1)
+    return list_answer
